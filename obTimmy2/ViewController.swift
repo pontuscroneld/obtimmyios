0620b5d9fb4c904be039134cb726c4dabeafb051
pontuscroneld <pontuscroneld@gmail.com>
2021-04-06 17:11:17 +0200

    Initial Commit

---
+++ obTimmy2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  obTimmy2
+//
+//  Created by Pontus Croneld on 2021-04-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
